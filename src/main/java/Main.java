a0c3277bcd3a0c53e3128d25282c27322f29482c
RealOrge <george.top@windowslive.com>
2020-09-01 10:01:46 +0300

    Create the account class with methods withdraw, deposit and testMethods

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import exercise.Account;
+
+public class Main {
+    public static void main(String[] args) {
+        Account account = new Account("me", 100, 0);
+        account.testMethods();
+    }
+}
---
+++ src/main/java/exercise/Account.java
@@ -0,0 +1,80 @@
+package exercise;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    private String owner;
+    private double balance;
+    private int numberOfTransactions;
+
+    public boolean deposit(double amount){
+        if (amount <= 0) return false;
+        this.balance += amount;
+        return true;
+    }
+
+    public boolean withdraw(double amount){
+        if ((amount < 0) || (amount > this.balance)) return false;
+        this.balance -= amount;
+        return true;
+    }
+
+    public void testMethods(){
+        System.out.println("Test deposit and withdraw functions for account balance: " + this.balance);
+        double testAmount = this.balance / 2;
+        int i = 1;
+
+        System.out.println("Test " + i);
+        System.out.println("Account balance: " + this.balance);
+        System.out.println("Try to deposit: " + testAmount);
+        System.out.println("The result should be a success");
+        if (deposit(testAmount)) System.out.println("You manage to deposit: " + testAmount);
+        else System.out.println("Failed at deposing: " + testAmount);
+
+        System.out.println(" ");
+        i++;
+        System.out.println("Test " + i);;
+        System.out.println("Account balance: " + this.balance);
+        testAmount = this.balance * -1;
+        System.out.println("Try to deposit: " + testAmount);
+        System.out.println("The result should be a failure");
+        if (deposit(testAmount)) System.out.println("You manage to deposit: " + testAmount);
+        else System.out.println("Failed at deposing: " + testAmount);
+
+        System.out.println(" ");
+        i++;
+        System.out.println("Test " + i);;
+        System.out.println("Account balance: " + this.balance);
+        testAmount = this.balance / 2;
+        System.out.println("Try to withdraw: " + testAmount);
+        System.out.println("The result should be a success");
+        if (withdraw(testAmount)) System.out.println("You manage to withdraw: " + testAmount);
+        else System.out.println("Failed at withdrawing: " + testAmount);
+
+        System.out.println(" ");
+        i++;
+        System.out.println("Test " + i);;
+        System.out.println("Account balance: " + this.balance);
+        testAmount = this.balance * -1;
+        System.out.println("Try to withdraw: " + testAmount);
+        System.out.println("The result should be a failure");
+        if (withdraw(testAmount)) System.out.println("You manage to withdraw: " + testAmount);
+        else System.out.println("Failed at withdrawing: " + testAmount);
+
+        System.out.println(" ");
+        i++;
+        System.out.println("Test " + i);;
+        System.out.println("Account balance: " + this.balance);
+        testAmount = this.balance + 1;
+        System.out.println("The result should be a failure");
+        System.out.println("Try to withdraw: " + testAmount);
+        if (withdraw(testAmount)) System.out.println("You manage to withdraw: " + testAmount);
+        else System.out.println("Failed at withdrawing: " + testAmount);
+    }
+}
