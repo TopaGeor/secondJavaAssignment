106d84972de0ac00f4fa588b31812e25cb1d0fc6
RealOrge <george.top@windowslive.com>
2020-09-01 22:58:33 +0300

    Add lombok

--- src/main/java/exerciseSecond/Product.java
+++ src/main/java/exerciseSecond/Product.java
@@ -1,5 +1,12 @@
 package exerciseSecond;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private int id;
     private String name;
