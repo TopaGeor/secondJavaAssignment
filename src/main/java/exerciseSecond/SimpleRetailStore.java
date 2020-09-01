686bee18ce14d73ca1f268cc39958487baf23d4f
RealOrge <george.top@windowslive.com>
2020-09-01 22:59:05 +0300

    Create SimpleRetailStore

---
+++ src/main/java/exerciseSecond/SimpleRetailStore.java
@@ -0,0 +1,28 @@
+package exerciseSecond;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleRetailStore implements IStore {
+    private double totalBuyBalance;
+    private double totalSellBalance;
+
+    @Override
+    public void buy(Product product) {
+        this.totalBuyBalance += product.getPriceWhenBuy();
+    }
+
+    @Override
+    public void sell(Product product) {
+        this.totalSellBalance += product.getPriceWhenSell();
+    }
+
+    @Override
+    public double getRevenue() {
+        return this.totalBuyBalance - this.totalSellBalance;
+    }
+}
